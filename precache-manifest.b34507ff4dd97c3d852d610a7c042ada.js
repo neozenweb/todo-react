9fd4cd0ecf61aaec2757e5f4e6484106eaba2d26
neozenweb <37545009+neozenweb@users.noreply.github.com>
2019-02-11 09:05:26 -0500

    Updates

---
+++ precache-manifest.b34507ff4dd97c3d852d610a7c042ada.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4ebad8e7a9f5566e1598",
+    "url": "/todo-react/static/js/runtime~main.4ebad8e7.js"
+  },
+  {
+    "revision": "dc6246b345d4eb93fbd6",
+    "url": "/todo-react/static/js/main.dc6246b3.chunk.js"
+  },
+  {
+    "revision": "371bad20edc5bb1f6fd1",
+    "url": "/todo-react/static/js/1.371bad20.chunk.js"
+  },
+  {
+    "revision": "dc6246b345d4eb93fbd6",
+    "url": "/todo-react/static/css/main.0ff7ce3d.chunk.css"
+  },
+  {
+    "revision": "2b57e2f6b1ce391e68a3cb9c753db0ec",
+    "url": "/todo-react/index.html"
+  }
+];
